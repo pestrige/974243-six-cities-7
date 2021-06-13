fcc283152710ccad50eb929a15af2f05cd2c5d86
pestrige <pestrige@gmail.com>
2021-06-13 09:56:20 +0300

    Исправляет форматирование даты

--- project/src/utils/common.js
+++ project/src/utils/common.js
@@ -1,5 +1,6 @@
 const MAX_RATING = 5;
 const MAX_PERSENTAGE = 100;
+const MONTH_SHIFT = 1;
 const Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 export const getPersentage = (rating) => rating * MAX_PERSENTAGE / MAX_RATING;
@@ -7,7 +8,7 @@ export const getPersentage = (rating) => rating * MAX_PERSENTAGE / MAX_RATING;
 export const formatDate = (date, isFull = false) => {
   const year = date.getFullYear();
   const rawMonth = date.getMonth();
-  const month = rawMonth < 10 ? `0${rawMonth}` : rawMonth;
+  const month = rawMonth < 10 ? `0${rawMonth + MONTH_SHIFT}` : rawMonth + MONTH_SHIFT;
   const day = date.getDate();
 
   return isFull ? `${Months[rawMonth]} ${year}` : `${year}-${month}-${day}`;
