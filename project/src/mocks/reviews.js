25443b5dfaa7e9565f2065819f4e4ac262d6ad36
pestrige <pestrige@gmail.com>
2021-06-13 10:07:32 +0300

    Убирает магическое число

--- project/src/mocks/reviews.js
+++ project/src/mocks/reviews.js
@@ -8,7 +8,7 @@ const REVIEW_ONE = {
   },
   'rating': 5,
   'comment': 'Bathed in the nature. Completely unplugged. Unforgettable.',
-  'date': '2021-05-19T09:55:44.854Z',
+  'date': '2021-01-19T09:55:44.854Z',
 };
 
 const REVIEW_TWO = {
@@ -21,7 +21,7 @@ const REVIEW_TWO = {
   },
   'rating': 3,
   'comment': 'The deluxe room was a quite comfortable one with all the adequate facilities. The only thing that made me feel uncomfortable was the rude behavior of an impolite staff at the reception desk.',
-  'date': '2021-05-19T09:55:44.854Z',
+  'date': '2021-09-19T09:55:44.854Z',
 };
 
 const REVIEW_THREE = {
@@ -34,7 +34,7 @@ const REVIEW_THREE = {
   },
   'rating': 3,
   'comment': 'The room was spacious and clean. The pool looked nothing like the photos and desparately needs a clean. The sauna and spa were closed for lunar new year holiday.',
-  'date': '2021-05-31T09:55:44.854Z',
+  'date': '2021-10-31T09:55:44.854Z',
 };
 
 export const REVIEWS = [
