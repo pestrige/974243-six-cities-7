921fd83458c71f48fccf651317905369afe4e4d6
pestrige <pestrige@gmail.com>
2021-06-25 08:28:00 +0300

    Оптимизирует ключи у офферов-заглушек

--- project/src/components/offers-loading/offers-loading.jsx
+++ project/src/components/offers-loading/offers-loading.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import CardLoading from './card-loading';
 
 const LOADING_OFFERS_COUNT = 6;
-const LOADING_OFFERS = new Array(LOADING_OFFERS_COUNT).fill('');
+const LOADING_OFFERS = new Array(LOADING_OFFERS_COUNT)
+  .fill('')
+  .map(() => Math.random());
 
 export default function OffersLoading() {
-  return LOADING_OFFERS.map(() => <CardLoading key={Math.random()} />);
+  return LOADING_OFFERS.map((id) => <CardLoading key={id} />);
 }
